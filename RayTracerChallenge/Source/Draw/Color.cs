4b98373b3e5fe384307119cdfe4ca6505bd3b6a1
Vladimir <limtis2109@gmail.com>
2022-10-07 17:24:52 +0200

    Changed /Tests structure; Added Colors;

---
+++ RayTracerChallenge/Source/Draw/Color.cs
@@ -0,0 +1,53 @@
+﻿namespace RT.Source.Draw
+{
+    public class Color
+    {
+        public float r;
+        public float g;
+        public float b;
+
+        public Color()
+        {
+            r = 0;
+            g = 0;
+            b = 0;
+        }
+
+        public Color(float r, float g, float b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        #region Operators
+
+        public static Color operator +(Color a, Color b) { return new Color(a.r + b.r, a.g + b.g, a.b + b.b); }
+
+        public static Color operator -(Color a, Color b) { return new Color(a.r - b.r, a.g - b.g, a.b - b.b); }
+
+        public static Color operator *(Color a, Color b) { return new Color(a.r * b.r, a.g * b.g, a.b * b.b); }
+
+        public static Color operator *(Color a, float b) { return new Color(a.r * b, a.g * b, a.b * b); }
+
+        public static bool operator ==(Color a, Color b) { return Calc.Equals(a.r, b.r) && Calc.Equals(a.g, b.g) && Calc.Equals(a.b, b.b); }
+
+        public static bool operator !=(Color a, Color b) { return !Calc.Equals(a.r, b.r) || !Calc.Equals(a.g, b.g) || !Calc.Equals(a.b, b.b); }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Color item)
+            {
+                return false;
+            }
+
+            return this == item;
+        }
+
+        public override int GetHashCode() { return HashCode.Combine(r, g, b); }
+
+        public override string ToString() { return $"Color(r:{r}, g:{g}, b:{b})"; }
+
+        #endregion
+    }
+}
---
+++ RayTracerTests/Tests/Draw/TestColors.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RT.Source.Draw;
+
+namespace RayTracerTests
+{
+    [TestClass]
+    public class TestColors
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+            Color c1 = new(0.9f, 0.6f, 0.75f);
+            Color c2 = new(0.7f, 0.1f, 0.25f);
+            Color expected = new(1.6f, 0.7f, 1.0f);
+            Assert.AreEqual(c1 + c2, expected);
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            Color c1 = new(0.9f, 0.6f, 0.75f);
+            Color c2 = new(0.7f, 0.1f, 0.25f);
+            Color expected = new(0.2f, 0.5f, 0.5f);
+            Assert.AreEqual(c1 - c2, expected);
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Color c1 = new(0.2f, 0.3f, 0.4f);
+            Color expected1 = new(0.4f, 0.6f, 0.8f);
+            Assert.AreEqual(c1 * 2, expected1);
+
+            Color c2 = new(1f, 0.2f, 0.4f);
+            Color c3 = new(0.9f, 1f, 0.1f);
+            Color expected2 = new(0.9f, 0.2f, 0.04f);
+            Assert.AreEqual(c2 * c3, expected2);
+        }
+    }
+}
